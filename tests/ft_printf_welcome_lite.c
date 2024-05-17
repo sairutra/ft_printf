c6659f26d3150cba42686aa2fff7e652c511aed1
sebastianpenning <whimizera@mailbox.org>
2024-05-17 07:06:47 +0200

    welcome lite normproof

--- tests/ft_printf_welcome_lite.c
+++ tests/ft_printf_welcome_lite.c
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_welcome_lite.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 07:05:59 by mynodeus          #+#    #+#             */
+/*   Updated: 2024/05/17 07:06:30 by mynodeus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printftests.h"
 #include <stdio.h>
 
-void welcome_lite()
+void	welcome_lite(void)
 {
 	printf(YEL "\nft_printf\n" RESET);
 }
 
-int main()
+int	main(void)
 {
 	welcome_lite();
-	return(0);
+	return (0);
 }
-
